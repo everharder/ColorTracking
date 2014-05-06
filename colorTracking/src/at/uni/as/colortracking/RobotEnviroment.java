a8fc71cb3cbf6c8f2051e2488b2f590bbddba99c
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2014-05-06 17:50:14 +0200

    Add calculations for self localization.

--- colorTracking/src/at/uni/as/colortracking/RobotEnviroment.java
+++ colorTracking/src/at/uni/as/colortracking/RobotEnviroment.java
@@ -5,7 +5,12 @@ import java.util.List;
 
 import org.opencv.core.Point;
 
+import android.util.Pair;
+
 public class RobotEnviroment {
+	private static final double MIN_X = 0;
+	private static final double MAX_X = 150;
+
 	private List<Beacon> beacons = new ArrayList<Beacon>();
 
 	public RobotEnviroment() {
@@ -20,7 +25,7 @@ public class RobotEnviroment {
 	}
 
 	public Point locate(List<TrackedObject> tracks) {
-		Point robot = null;	
+		Point robot = null;
 		List<TrackedObject> beacons = new ArrayList<TrackedObject>();
 
 		// Get all beacons (objects with 2 tracked colors).
@@ -29,16 +34,49 @@ public class RobotEnviroment {
 				beacons.add(t);
 
 		if (beacons.size() > 1) {
-			// Determine intersection of two circles.
-			List<Point> intersects = CircleCut.circleIntersect(beacons.get(0)
-					.getCoherentDistanceNearest().second, beacons.get(1)
-					.getCoherentDistanceNearest().second, beacons.get(0).getCoords(),
-					beacons.get(1).getCoords());
+			// Get distances to beacons.
+			Pair<Point, Double> d0 = beacons.get(0)
+					.getCoherentDistanceNearest();
+			Pair<Point, Double> d1 = beacons.get(1)
+					.getCoherentDistanceNearest();
+			// Get coordinates of beacons.
+			Point p0 = beacons.get(0).getCoords();
+			Point p1 = beacons.get(1).getCoords();
 
-			System.out.println("Point 0: " + intersects.get(0) + "\t Point 1: "
-					+ intersects.get(1));
+			if (d0 == null || d1 == null || p0 == null || p1 == null)
+				return null;
+
+			// Determine intersection of two circles.
+			List<Point> intersects = CircleCut.circleIntersect(d0.second,
+					d1.second, p0, p1);
+			// Determine middle point of intersection.
+			Point m = new Point(Math.abs(intersects.get(0).x
+					- intersects.get(1).x), Math.abs(intersects.get(0).y
+					- intersects.get(1).y));
+
+			// Case LEFT or RIGHT
+			if ((p0.x == MIN_X && p1.x == MIN_X)
+					|| (p0.x == MAX_X && p1.x == MAX_X)) {
+				// Determine distance between beacon and Point m.
+				double l = Math.sqrt(Math.pow(m.x, 2)
+						+ Math.pow(Math.abs(m.y - p0.y), 2));
+				// Determine distance between robot and Point m.
+				double lr = Math.sqrt(Math.pow(d0.second, 2) - Math.pow(l, 2));
+
+				robot = new Point(Math.abs(m.x - lr), m.y);
+			}
+			// Case UP or DOWN
+			else {
+				// Determine distance between beacon and Point m.
+				double l = Math.sqrt(Math.pow(Math.abs(m.x - p0.x), 2)
+						+ Math.pow(m.y, 2));
+				// Determine distance between robot and Point m.
+				double lr = Math.sqrt(Math.pow(d0.second, 2) - Math.pow(l, 2));
+
+				robot = new Point(m.x, Math.abs(m.y - lr));
+			}
 		}
-		
+
 		return robot;
 	}
 }
