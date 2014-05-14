7cc3962df2225bc1178fa65dbf7750568cd2fe61
daniel <eberharter.daniel@gmail.com>
2014-05-14 16:22:16 +0200

    add tolerance for each color

--- colorTracking/src/at/uni/as/colortracking/tracking/Color.java
+++ colorTracking/src/at/uni/as/colortracking/tracking/Color.java
@@ -3,22 +3,25 @@ package at.uni.as.colortracking.tracking;
 import org.opencv.core.Scalar;
 
 public enum Color {
-	RED		(new Scalar(255,   0,   0)),
-	BLUE	(new Scalar(  0,   0, 255)),
-	YELLOW	(new Scalar(255, 255,   0)),
-	WHITE	(new Scalar(255, 255, 255));
+	RED		(new Scalar(255,   0,   0), new Scalar( 10, 100, 100)),
+	BLUE	(new Scalar(  0,   0, 255), new Scalar( 10, 120, 120)),
+	YELLOW	(new Scalar(255, 255,   0), new Scalar( 10,  50,  50)),
+	WHITE	(new Scalar(255, 255, 255), new Scalar(  0,   0,   0));
 	
 	private final Scalar hsv;
 	private final Scalar rgb;
+	private final Scalar tol;
 	
-	Color(Scalar hsv, Scalar rgb) {
+	Color(Scalar hsv, Scalar rgb, Scalar tol) {
 		this.hsv = hsv;
 		this.rgb = rgb;
+		this.tol = tol;
 	}
 	
-	Color(Scalar rgb){
+	Color(Scalar rgb, Scalar tol){
 		this.rgb = rgb;
 		this.hsv = ColorTrackingUtil.convertRGB2HSV(rgb);
+		this.tol = tol;
 	}
 	
 	public Scalar hsv() {
@@ -27,5 +30,9 @@ public enum Color {
 	
 	public Scalar rgb() {
 		return rgb;
+	}
+	
+	public Scalar tol() {
+		return tol;
 	}
 }
--- colorTracking/src/at/uni/as/colortracking/tracking/ColorTrackingUtil.java
+++ colorTracking/src/at/uni/as/colortracking/tracking/ColorTrackingUtil.java
@@ -17,12 +17,12 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 public class ColorTrackingUtil {
-	private static final int BLUR_FACTOR = 9; // needs to be odd
+	private static final int BLUR_FACTOR = 11; // needs to be odd
 	public static int FOREGROUND_TOLERANCE_H = 25;
 	public static int FOREGROUND_TOLERANCE_S = 50;
 	public static int FOREGROUND_TOLERANCE_V = 50;
 
-	public static final Scalar DEFAULT_TOL_HSV = new Scalar(10, 125, 125);
+	public static final Scalar DEFAULT_TOL_HSV = new Scalar(10, 120, 120);
 	private static final double DETECTION_AREA_MIN = 500;
 	private static final int TRACKED_RECT_THICKNESS = 3;
 
@@ -96,162 +96,6 @@ public class ColorTrackingUtil {
 		return homography;
 	}
 
-	/**
-	 * Extracts the foreground picture.
-	 * 
-	 * @param x
-	 *            X-Coordinate of touched point.
-	 * @param y
-	 *            Y-Coordinate of touched point.
-	 * @param image
-	 *            Matrix, which contains image with RGBA format.
-	 * @return
-	 */
-	public static Mat getForegroundImage(int x, int y, Mat image) {
-		int cols = image.cols();
-		int rows = image.rows();
-
-		// sample rectangle
-		Rect touchedRect = new Rect();
-		touchedRect.x = (x > 4) ? x - 4 : 0;
-		touchedRect.y = (y > 4) ? y - 4 : 0;
-		touchedRect.width = (x + 4 < cols) ? x + 4 - touchedRect.x : cols
-				- touchedRect.x;
-		touchedRect.height = (y + 4 < rows) ? y + 4 - touchedRect.y : rows
-				- touchedRect.y;
-		Mat touchedRegionRgba = image.submat(touchedRect);
-
-		Mat touchedRegionHsv = new Mat();
-		Mat mEmpty = new Mat();
-		Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv,
-				Imgproc.COLOR_RGB2HSV_FULL);
-
-		// Calculate average color of touched region
-		Scalar hsvColor = Core.sumElems(touchedRegionHsv);
-		;
-		int pointCount = touchedRect.width * touchedRect.height;
-		for (int i = 0; i < hsvColor.val.length; i++)
-			hsvColor.val[i] /= pointCount;
-
-		// tolerances
-		Scalar mColorRadius = new Scalar(FOREGROUND_TOLERANCE_H,
-				FOREGROUND_TOLERANCE_S, FOREGROUND_TOLERANCE_V, 0);
-
-		double minH = (hsvColor.val[0] - mColorRadius.val[0] > 0) ? hsvColor.val[0]
-				- mColorRadius.val[0]
-				: 255 - hsvColor.val[0] - mColorRadius.val[0];
-		double maxH = (hsvColor.val[0] + mColorRadius.val[0] < 255) ? hsvColor.val[0]
-				+ mColorRadius.val[0]
-				: hsvColor.val[0] + mColorRadius.val[0] - 255;
-
-		Mat mPyrDownMat = new Mat();
-		Mat mHsvMat = new Mat();
-		Mat mMask = new Mat();
-		Mat mDilatedMask = new Mat();
-		Mat mHierarchy = new Mat();
-
-		// image processing
-		Imgproc.pyrDown(image, mPyrDownMat);
-		Imgproc.pyrDown(mPyrDownMat, mPyrDownMat);
-		Imgproc.cvtColor(mPyrDownMat, mHsvMat, Imgproc.COLOR_RGB2HSV_FULL);
-
-		// get binary image from ranges
-		Scalar mLowerBound = new Scalar(0);
-		Scalar mUpperBound = new Scalar(0);
-		mLowerBound.val[1] = hsvColor.val[1] - mColorRadius.val[1];
-		mUpperBound.val[1] = hsvColor.val[1] + mColorRadius.val[1];
-		mLowerBound.val[2] = hsvColor.val[2] - mColorRadius.val[2];
-		mUpperBound.val[2] = hsvColor.val[2] + mColorRadius.val[2];
-		mLowerBound.val[3] = 0;
-		mUpperBound.val[3] = 255;
-
-		if (minH < maxH) {
-			mLowerBound.val[0] = minH;
-			mUpperBound.val[0] = maxH;
-			Core.inRange(mHsvMat, mLowerBound, mUpperBound, mMask);
-		} else {
-			Mat min = new Mat();
-			Mat max = new Mat();
-
-			mLowerBound.val[0] = minH;
-			mUpperBound.val[0] = 255;
-			Core.inRange(mHsvMat, mLowerBound, mUpperBound, min);
-
-			mLowerBound.val[0] = 0;
-			mUpperBound.val[0] = maxH;
-			Core.inRange(mHsvMat, mLowerBound, mUpperBound, max);
-
-			Core.bitwise_or(min, max, mMask);
-		}
-		mDilatedMask = ColorTrackingUtil.segment(mMask);
-
-		// finding contours in image
-		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Imgproc.findContours(mDilatedMask, contours, mHierarchy,
-				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-		// Find max contour area
-		double maxArea = 0;
-		MatOfPoint biggest = null;
-		for (MatOfPoint c : contours) {
-			double area = Imgproc.contourArea(c);
-			if (area > maxArea) {
-				maxArea = area;
-				Core.multiply(c, new Scalar(4, 4), c);
-				biggest = c;
-			}
-		}
-
-		Mat cli = null;
-		if (biggest != null)
-			cli = image.submat(Imgproc.boundingRect(biggest));
-
-		// releasing memory
-		for (MatOfPoint c : contours)
-			c.release();
-		touchedRegionRgba.release();
-		touchedRegionHsv.release();
-		mPyrDownMat.release();
-		mHsvMat.release();
-		mMask.release();
-		mDilatedMask.release();
-		mHierarchy.release();
-		mEmpty.release();
-
-		return cli;
-	}
-
-	/**
-	 * Segments the image with eroding, dilating and smoothing.
-	 * 
-	 * @param mImg
-	 *            Image.
-	 * @return Segmented image.
-	 */
-	public static Mat segment(Mat img) {
-		Imgproc.erode(img, img, new Mat());
-		Imgproc.dilate(img, img, new Mat());
-		Imgproc.medianBlur(img, img, BLUR_FACTOR);
-
-		return img;
-	}
-
-	/**
-	 * Segments all images in the list with eroding, dilating and smoothing.
-	 * 
-	 * @param imgs
-	 *            List of images.
-	 * @return List of segmented images.
-	 */
-	public static List<Mat> segment(List<Mat> imgs) {
-		List<Mat> segments = new ArrayList<Mat>();
-
-		for (Mat m : imgs)
-			segments.add(segment(m));
-
-		return segments;
-	}
-
 	public static List<TrackedColor> detectColor(Mat img, Color color,
 			Scalar tolerance) {
 
@@ -271,7 +115,7 @@ public class ColorTrackingUtil {
 		Core.inRange(imgHsv, lowerBound, upperBound, imgBinary);
 
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Imgproc.erode(imgBinary, imgBinary, new Mat());
+		Imgproc.dilate(imgBinary, imgBinary, new Mat());
 		Imgproc.medianBlur(imgBinary, imgBinary, BLUR_FACTOR);
 		Imgproc.findContours(imgBinary, contours, new Mat(),
 				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
@@ -305,8 +149,7 @@ public class ColorTrackingUtil {
 
 		Map<Color, List<TrackedColor>> detectedObjects = new HashMap<Color, List<TrackedColor>>();
 		for (Color c : Color.values()) {
-			List<TrackedColor> l = ColorTrackingUtil.detectColor(hsv, c,
-					ColorTrackingUtil.DEFAULT_TOL_HSV);
+			List<TrackedColor> l = ColorTrackingUtil.detectColor(hsv, c, c.tol());
 
 			if (l != null && l.size() > 0)
 				detectedObjects.put(c, l);
